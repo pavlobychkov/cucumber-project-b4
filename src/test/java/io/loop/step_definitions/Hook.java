9dce53d234308eca1195b3961748f7f8fb5107b7
Pavlo Bychkov <pavlobychkov87@gmail.com>
2025-02-13 22:07:02 -0500

    Cucumber intro

---
+++ src/test/java/io/loop/step_definitions/Hook.java
@@ -0,0 +1,17 @@
+package io.loop.step_definitions;
+
+import io.cucumber.java.*;
+import io.loop.utilities.Driver;
+
+public class Hook {
+
+    @Before
+    public void setUp(){
+        Driver.getDriver();
+    }
+
+    @After
+    public void tearDown(){
+        Driver.closeDriver();
+    }
+}
---
+++ src/test/java/io/loop/utilities/GeneralConstants.java
@@ -0,0 +1,6 @@
+package io.loop.utilities;
+
+public class GeneralConstants {
+    public static final String BROWSER = "chrome";
+    public static final String DOCUPORT_TEST = "https://beta.docuport.app";
+}
